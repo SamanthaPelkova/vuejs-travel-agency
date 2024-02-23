408d874013d187f9b6c88a50ddac60faa757e0f5
samantha <samanthapelkova1@gmail.com>
2024-02-23 10:37:12 +0100

    some changes

--- src/components/Services/TravelServices.vue
+++ src/components/Services/TravelServices.vue
@@ -1,12 +1,6 @@
 <template>
-  <h2 class="offer">{{ offerTitle }}</h2>
-  <p class="offer-subtitle">{{ offerSubtitle }}</p>
-  <div class="travel-services" v-for="service in services" :key="service.id">
-    <p class="service-label">{{ service.label }}</p>
-    <h3 class="service-title">{{ service.title }}</h3>
-    <img :src="service.image" class="service-image" alt="">
-    <p class="service-description">{{ service.description }}</p>
-  </div>
+  <h2 class="offer">{{  }}</h2>
+
   <footer>
     © 2024 TravelAgency. All rights reserved.
   </footer>
@@ -15,64 +9,9 @@
 <script>
 
 
-
-import {ref} from "vue";
-import image1 from "@/pictures/sauna.jpg";
-import image2 from "@/pictures/massage.jpg";
-import image3 from "@/pictures/yoga.jpg";
-import image4 from "@/pictures/work-out.jpg";
-import image5 from "@/pictures/swimming-pool.jpg";
-
 export default {
   name: 'TravelServices',
-  setup(){
-    const services = ref([
-      {
-        id: 1,
-        title: 'Sauna',
-        description: 'fioejfojfj eojf ioejfij ief jio jj ifji iofj o ifjij iojei jjioefj ijeij ioj eiji ju  Lorem Ipsum er rett og slett dummytekst fra og for trykkeindustrien. Lorem Ipsum har vært bransjens standard for dummytekst helt siden 1500-tallet, da en ukjent boktrykker stokket en mengde bokstaver for å lage et prøveeksemplar av en bok. m tok i bruk nettopp Lorem Ipsum for dummytekst.Lorem Ipsum er rett og slett dummytekst fra og for trykkeindustrien. Lorem Ipsum har vært bransjens standard for dummytekst helt siden 1500-tallet,llegg til å be un nijeju  on iji',
-        label: 'Recently added',
-        image: image1
-      },
-      {
-        id: 2,
-        title: 'Masáže',
-        description: 'fioejfojfj eLorem Ipsum er rett og slett dummytekst fra og for trykkeindustrien. Lorem Ipsum har vært bransjens standard for dummytekst helt siden 1500-tallet, da en ukjent boktrykker stokket en mengde bokstaver for å lage et prøveeksemplar av en bok. m tok i bruk nettopp Lorem Ipsum for dummytekst.Lorem Ipsum er rett og slett dummytekst fra og for trykkeindustrien. Lorem Ipsum har vært bransjens standard for dummytekst helt siden 1500-tallet,llegg til å beojf ioejfij ief jio jj ifji iofj o ifjij iojei jjioefj ijeij ioj eiji ju un nijeju  on iji',
-        label: 'Lowest price',
-        image: image2
-      },
-      {
-        id: 3,
-        title: 'Yoga',
-        description: 'fioejfojfj eojf ioeLorem Ipsum er rett og slett dummytekst fra og for trykkeindustrien. Lorem Ipsum har vært bransjens standard for dummytekst helt siden 1500-tallet, da en ukjent boktrykker stokket en mengde bokstaver for å lage et prøveeksemplar av en bok. m tok i bruk nettopp Lorem Ipsum for dummytekst.Lorem Ipsum er rett og slett dummytekst fra og for trykkeindustrien. Lorem Ipsum har vært bransjens standard for dummytekst helt siden 1500-tallet,llegg til å bejfij ief jio jj ifji iofj o ifjij iojei jjioefj ijeij ioj eiji ju un nijeju  on iji',
-        label: 'Discount 230CZK',
-        image: image3
-      },
-      {
-        id: 4,
-        title: 'Cvičení',
-        description: 'fioejfojfj eojf ioeLorem Ipsum er rett og slett dummytekst fra og for trykkeindustrien. Lorem Ipsum har vært bransjens standard for dummytekst helt siden 1500-tallet, da en ukjent boktrykker stokket en mengde bokstaver for å lage et prøveeksemplar av en bok. m tok i bruk nettopp Lorem Ipsum for dummytekst.Lorem Ipsum er rett og slett dummytekst fra og for trykkeindustrien. Lorem Ipsum har vært bransjens standard for dummytekst helt siden 1500-tallet,llegg til å bejfij ief jio jj ifji iofj o ifjij iojei jjioefj ijeij ioj eiji ju un nijeju  on iji',
-        label: '1 + 1 free',
-        image: image4
-      },
-      {
-        id: 5,
-        title: 'Plavecký bazén',
-        description: 'fioejfojfj eojf ioeLorem Ipsum er rett og slett dummytekst fra og for trykkeindustrien. Lorem Ipsum har vært bransjens standard for dummytekst helt siden 1500-tallet, da en ukjent boktrykker stokket en mengde bokstaver for å lage et prøveeksemplar av en bok. m tok i bruk nettopp Lorem Ipsum for dummytekst.Lorem Ipsum er rett og slett dummytekst fra og for trykkeindustrien. Lorem Ipsum har vært bransjens standard for dummytekst helt siden 1500-tallet,llegg til å bejfij ief jio jj ifji iofj o ifjij iojei jjioefj ijeij ioj eiji ju un nijeju  on iji',
-        label: 'Recently added',
-        image: image5
-      }
-    ])
-
-    const offerTitle = ref('Our Services')
-    const offerSubtitle = ref('Lorem Ipsum er rett og slett dummytekst fra og for trykkeindustrien. Lorem Ipsum har vært bransjens standard for dummytekst helt siden 1500-tallet, da en ukjent boktrykker stokket en mengde bokstaver for å lage et prøveeksemplar av en bok. m tok i bruk nettopp Lorem Ipsum for dummytekst.Lorem Ipsum er rett og slett dummytekst fra og for trykkeindustrien. Lorem Ipsum har vært bransjens standard for dummytekst helt siden 1500-tallet,llegg til å be. Lorem Ipsum er rett og slett dummytekst fra og for trykkeindustrien. Lorem Ipsum har vært bransjens standard for dummytekst helt siden 1500-tallet, da en ukjent boktrykker stokket en mengde bokstaver for å lage et prøveeksemplar av en bok. m tok i bruk nettopp Lorem Ipsum for dummytekst.Lorem Ipsum er rett og slett dummytekst fra og for trykkeindustrien. Lorem Ipsum har vært bransjens standard for dummytekst helt siden 1500-tallet,llegg til å be.Lorem Ipsum er rett og slett dummytekst fra og for trykkeindustrien. Lorem Ipsum har vært bransjens standard for dummytekst helt siden 1500-tallet, da en ukjent boktrykker stokket en mengde bokstaver for å lage et prøveeksemplar av en bok. m tok i bruk nettopp Lorem Ipsum for dummytekst.Lorem Ipsum er rett og slett dummytekst fra og for trykkeindustrien. Lorem Ipsum har vært bransjens standard for dummytekst helt siden 1500-tallet,llegg til å be')
-
-
-    return {
-      services,
-      offerTitle,
-      offerSubtitle
-    }
+  setup() {
   }
 
 }
--- src/components/ShoppingBasket/ShoppingBasket.vue
+++ src/components/ShoppingBasket/ShoppingBasket.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="shopping-basket">
-    <h2></h2>
+      <h1 class="blue-space">------</h1>
+    <div class="title-basket">
+      <h2 class="shopping-basket-title">Your Shopping Basket</h2>
+    </div>
   </div>
 </template>
 
@@ -21,6 +24,27 @@ export default {
 </script>
 
 <style>
+.blue-space {
+  background: #3ACBE8;
+  position: absolute;
+  padding: 30px;
+  color: #3ACBE8;
+  margin-top: -90px;
+  margin-left: 140px;
+  width: 34%;
+
+}
+
+.shopping-basket-title {
+  background: linear-gradient(to right, #172c64, #2ca3b9);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;  font-size: 55px;
+  padding: 20px;
+  position: absolute;
+  float: left;
+  margin-top: -90px;
+  margin-left: 160px;
+}
 
 
 </style>
