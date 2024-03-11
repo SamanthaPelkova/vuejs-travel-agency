f70da0ee1ec6f53ba0fbdc80e7c4e4a74caa332b
samantha <samanthapelkova1@gmail.com>
2024-03-11 10:02:18 +0100

    some changes

--- src/components/ShoppingBasket/ShoppingBasket.vue
+++ src/components/ShoppingBasket/ShoppingBasket.vue
@@ -5,6 +5,14 @@
       <h2 class="shopping-basket-title">Your Shopping Basket</h2>
     </div>
   </div>
+  <div class="shopping-basket-information">
+    <h3 class="shopping-basket-subtitle number">Number</h3>
+    <h3 class="shopping-basket-subtitle product">Product</h3>
+    <h3 class="shopping-basket-subtitle descr">Description</h3>
+    <h3 class="shopping-basket-subtitle ks">/ks</h3>
+    <h3 class="shopping-basket-subtitle size">Size</h3>
+    <h3 class="shopping-basket-subtitle price">Price</h3>
+  </div>
 </template>
 
 <script>
@@ -38,12 +46,49 @@ export default {
 .shopping-basket-title {
   background: linear-gradient(to right, #172c64, #2ca3b9);
   -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;  font-size: 55px;
+  -webkit-text-fill-color: transparent;
+  font-size: 40px;
   padding: 20px;
   position: absolute;
   float: left;
   margin-top: -90px;
-  margin-left: 160px;
+  margin-left: 120px;
+}
+
+.shopping-basket-information {
+  display: flex;
+}
+
+.shopping-basket-subtitle {
+  margin-top: 50px;
+  font-size: 25px;
+  color: #3ACBE8;
+  padding: 10px;
+  background-color: white;
+}
+
+.number {
+  padding-left: 30px;
+}
+
+.product {
+  padding-left: 30px;
+}
+
+.descr {
+  padding-left: 40px;
+}
+
+.ks {
+  padding-left: 350px;
+}
+
+.size {
+  padding-left: 30px;
+}
+
+.price {
+  padding-left: 30px;
 }
 
 
