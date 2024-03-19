a43a0046c2e369c6c06f382a36f1823ef88e2122
samantha <samanthapelkova1@gmail.com>
2024-03-19 11:22:12 +0100

    some changes

--- src/components/HomePage/HomePage-Country-Customs/CustomsSpain.vue
+++ src/components/HomePage/HomePage-Country-Customs/CustomsSpain.vue
@@ -3,12 +3,12 @@
     <h1 class="customs-spain-title">Španělsko</h1>
     <h4>Lorem Ipsum er rett og slett dummytekst fra og for trykkeindustrien. Lorem I ek nettopp Lorem Ipsum for dummytekst.Lorem Ipsum er rett og slett dummytekst fra og for trykkeindustrien. Lorem Ipsum har vært bransjens standard for dummytekst helt siden 1500-tallet,llegg til å be</h4>
     <div class="customs-spain">
-      <li>Siesta: Krátká odpočinková pauza během odpoledne, obvykle mezi 14:00 a 17:00 hodinou, kdy se mnoho obchodů a firem zavírá a lidé mají čas na odpočinek nebo oběd.</li>
-      <li>Tapas: Tradiční španělské jednohubky, které se podávají jako předkrm nebo jako součást společného stolování. Je běžné jít s přáteli na "tapeo", což je procházka mezi bary a konzumace různých tapas.</li>
-      <li>Bullfighting (Corrida de toros): Tradiční španělská sportovní událost, která se odehrává v aréně. Tato tradice je však v poslední době kontroverzní a je méně populární než dříve.</li>
-      <li>Španělská kuchyně: Zahrnuje mnoho regionálních specialit, jako jsou paella, gazpacho, tortilla espanola, churros s horkou čokoládou a mnoho dalších.</li>
-      <li>Sociální interakce: Ve španělské kultuře je běžné trávit čas s rodinou a přáteli. Večerní procházky (paseo) a společné večeře jsou důležitými součástmi sociálního života.</li>
-      <li>Fiestas: Tradiční oslavy a festivaly, které se konají po celý rok v různých částech Španělska. Tyto fiesty mohou být náboženského, kulturního nebo historického původu a jsou často spojeny s hudbou, tancem a jídlem.</li>
+      <li><b>Siesta:</b> Krátká odpočinková pauza během odpoledne, obvykle mezi 14:00 a 17:00 hodinou, kdy se mnoho obchodů a firem zavírá a lidé mají čas na odpočinek nebo oběd.</li>
+      <li><b>Tapas:</b> Tradiční španělské jednohubky, které se podávají jako předkrm nebo jako součást společného stolování. Je běžné jít s přáteli na "tapeo", což je procházka mezi bary a konzumace různých tapas.</li>
+      <li><b>Bullfighting (Corrida de toros):</b> Tradiční španělská sportovní událost, která se odehrává v aréně. Tato tradice je však v poslední době kontroverzní a je méně populární než dříve.</li>
+      <li><b>Španělská kuchyně:</b> Zahrnuje mnoho regionálních specialit, jako jsou paella, gazpacho, tortilla espanola, churros s horkou čokoládou a mnoho dalších.</li>
+      <li><b>Sociální interakce:</b> Ve španělské kultuře je běžné trávit čas s rodinou a přáteli. Večerní procházky (paseo) a společné večeře jsou důležitými součástmi sociálního života.</li>
+      <li><b>Fiestas:</b> Tradiční oslavy a festivaly, které se konají po celý rok v různých částech Španělska. Tyto fiesty mohou být náboženského, kulturního nebo historického původu a jsou často spojeny s hudbou, tancem a jídlem.</li>
     </div>
     <img alt="#" src="../../../pictures/icons/spain-customs.jpeg" class="image-customs-spain">
   </div>
@@ -37,15 +37,15 @@ export default {
 
 <style>
 .spain-customs-body {
-  background: linear-gradient(to top, #172c64, #0047AB);
+  background: linear-gradient(to left, #172c64, #0047AB);
   border-radius: 80px;
   width: 80%;
   margin-left: 160px;
-  box-shadow: 3px 3px 30px 3px #172c64;
+  box-shadow: 3px 3px 80px 3px #172c64;
   margin-top: 40px;
   color: white;
   padding: 30px;
-  margin-bottom: 60px;
+  margin-bottom: 120px;
 }
 
 .customs-spain-title {
@@ -59,6 +59,7 @@ export default {
   position: absolute;
   margin-left: 60px;
   margin-top: -500px;
+  box-shadow: 3px 3px 50px 3px #172c64;
 }
 
 .customs-spain {
